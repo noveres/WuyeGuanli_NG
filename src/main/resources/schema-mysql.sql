a6ac4a6081a719bccec12b516865769e3d9ce1f0
LAPTOP-68SOCEF2\user <w>
2025-03-03 15:27:45 +0800

    更新fee_info table

--- src/main/resources/schema-mysql.sql
+++ src/main/resources/schema-mysql.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `report` IF NOT EXISTS (
+CREATE TABLE  IF NOT EXISTS `report`(
   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
   `sort` varchar(20) NOT NULL COMMENT '''種類:比如電梯、大門''',
   `where` varchar(20) NOT NULL COMMENT '''哪裡:比如A棟3F''',
@@ -12,3 +12,11 @@ CREATE TABLE `report` IF NOT EXISTS (
   `cost` int unsigned NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE IF NOT EXISTS `fee_info` (
+  `address` varchar(45) NOT NULL,
+  `other` varchar(445) DEFAULT NULL COMMENT '''["1134是","1141是"1142否只繳一半"]''',
+  `modifying_date` date DEFAULT NULL COMMENT '更新一次刷新一次',
+  PRIMARY KEY (`address`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='管理費';

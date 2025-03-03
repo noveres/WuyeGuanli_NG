45e1310fdd4de752ec4d99afcc875a8ac9f62014
noveres <andrew901117@gmail.com>
2025-03-03 20:03:48 +0800

    sqlupdata3/3 8.

--- src/main/resources/schema-mysql.sql
+++ src/main/resources/schema-mysql.sql
@@ -43,3 +43,35 @@ CREATE TABLE IF NOT EXISTS `resident_information` (
   PRIMARY KEY (`Partitionhousenumber`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+CREATE TABLE IF NOT EXISTS `user` (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    identity_number VARCHAR(20),
+    password VARCHAR(255) NOT NULL,
+    role ENUM('admin', 'landlord', 'tenant') NOT NULL,
+    landlord_id INT NULL,
+    is_currently_residing BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT fk_landlord FOREIGN KEY (landlord_id) REFERENCES Users(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+DROP TRIGGER IF EXISTS before_users_update;
+
+CREATE TRIGGER before_users_update
+    BEFORE UPDATE ON Users
+    FOR EACH ROW
+    SET NEW.updated_at = CURRENT_TIMESTAMP;
+
+INSERT INTO Users (name, identity_number, password, role, landlord_id) VALUES
+('系統管理員', 'A000000001', 'adminpassword', 'admin', NULL),
+('房東1', 'B000000001', 'landlordpassword', 'landlord', NULL),
+('房東2', 'B000000002', 'landlordpassword', 'landlord', NULL),
+('房客1', 'C000000001', 'C000000001', 'tenant', 2),
+('房客2', 'C000000002', 'C000000002', 'tenant', 2),
+('房客3', 'C000000003', 'C000000003', 'tenant', 3),
+('房客4', 'C000000004', 'C000000004', 'tenant', 3),
+('房客5', 'C000000005', 'C000000005', 'tenant', 2),
+('房客6', 'C000000006', 'C000000006', 'tenant', 3),
+('房客7', 'C000000007', 'C000000007', 'tenant', 3);
+
+

c11b5b022a5ce0e079af4c4ff8e5b61f85325882
LAPTOP-68SOCEF2\user <w>
2025-03-04 16:44:43 +0800

    update fee

--- src/main/java/com/example/WuyeGuanli/service/ifs/FeeInfoService.java
+++ src/main/java/com/example/WuyeGuanli/service/ifs/FeeInfoService.java
@@ -1,5 +1,7 @@
 package com.example.WuyeGuanli.service.ifs;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.example.WuyeGuanli.entity.FeeInfo;
@@ -7,5 +9,8 @@ import com.example.WuyeGuanli.vo.BasicRes;
 
 
 public interface FeeInfoService {
-	public BasicRes saveFeeInfo(FeeInfo feeInfo);
+	public boolean saveFeeInfo(FeeInfo feeInfo);
+	
+	public	List<FeeInfo> getAllFeeInfo();
+	
 }

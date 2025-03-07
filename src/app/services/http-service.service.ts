030c5b3b7fa8c161f6720bc54f43179f7e4e4915
黃 <w>
2025-03-07 17:18:59 +0800

    Revert "http-"
    This reverts commit bdb2f4841316232658ad7308d7b4a356871f84d9.

--- src/app/services/http-service.service.ts
+++ src/app/services/http-service.service.ts
@@ -1,27 +1,34 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpServiceService {
+  private baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
   GetApi<T>(url: string): Observable<T> {
-    return this.http.get<T>(url);
+    return this.http.get<T>(`${this.baseUrl}${url}`);
   }
 
   PostApi<T>(url: string, PostData: any): Observable<T> {
-    return this.http.post<T>(url, PostData);
+    return this.http.post<T>(`${this.baseUrl}${url}`, PostData);
   }
 
   PutApi<T>(url: string, PostData: any): Observable<T> {
-    return this.http.put<T>(url, PostData);
+    return this.http.put<T>(`${this.baseUrl}${url}`, PostData);
   }
 
   DeleteApi<T>(url: string): Observable<T> {
-    return this.http.delete<T>(url);
+    return this.http.delete<T>(`${this.baseUrl}${url}`);
+  }
+
+  // 獲取靜態資源的完整URL
+  getStaticUrl(path: string): string {
+    return `${this.baseUrl}${path}`;
   }
 }

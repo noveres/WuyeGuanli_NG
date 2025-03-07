5e9c15b6bd347eae32c2e500596ee23b588b9372
gouz2807 <gouz2807@gmail.com>
2025-03-07 09:58:16 +0800

    DashboardService update

--- src/main/java/com/example/WuyeGuanli/controller/DashBoardServiceController.java
+++ src/main/java/com/example/WuyeGuanli/controller/DashBoardServiceController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DashBoardServiceController {
+
     @Autowired
     private DashBoardService dashBoardService;
 

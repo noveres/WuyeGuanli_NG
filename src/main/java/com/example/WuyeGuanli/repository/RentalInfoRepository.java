0fbe3b5efa40d1fe1d1a5a1aac204909a43b527b
andrew <andrew901117@gmail.com>
2025-03-13 05:11:36 +0800

    刪掉了多出來的後端????? 儀表板設計

--- src/main/java/com/example/WuyeGuanli/repository/RentalInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.WuyeGuanli.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.WuyeGuanli.entity.RentalInfo;
-
-public interface RentalInfoRepository extends JpaRepository<RentalInfo, Integer>{
-
-}
--- src/main/java/com/example/WuyeGuanli/service/ifs/FinancialService.java
+++
@@ -1,9 +0,0 @@
-package com.example.WuyeGuanli.service.ifs;
-
-import com.example.WuyeGuanli.vo.BasicRes;
-import com.example.WuyeGuanli.vo.FinancialAddInfoReq;
-
-public interface FinancialService {
-	
-	public int addInfo(FinancialAddInfoReq req); 
-}
